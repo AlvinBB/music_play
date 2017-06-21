75ee3601894b95fe4415eb76f6290a83945a08f4
AlvinBB <764199809@qq.com>
2017-06-22 00:03:22 +0800

    modify catalog

--- server.js
+++ server.js
@@ -5,13 +5,14 @@
 const http = require("http")
 const express = require("express")
 //const qs = require("querystring")
-const pool = require("./pool")
-const user = require("./user")
-const music = require("./music")
+const pool = require("./lib/pool")
+const user = require("./lib/user")
+const music = require("./lib/music")
 
 let app = express()
 let srv = http.createServer(app).listen(8080)
 
+/*********处理各种请求********/
 //静态资源用express中间件处理
 app.use(express.static("public"))
 app.get('/',(req,res)=>{
